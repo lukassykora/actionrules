ef5278a4accb22c1b55746873399ea8033477e90
Lukas Sykora <lukas.sykora@wunderman.com>
2019-10-05 00:50:22 +0200

    Right confidence (removed rules that are not full)

--- arules/arules.py
+++ arules/arules.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from typing import List
 import itertools
-import numpy as np
 from desiredstate import DesiredState
 
 
@@ -20,7 +19,7 @@ class ActionRules:
         self.flexible_tables = flexible_tables
         self.decision_tables = decision_tables
         self.desired_state = desired_state
-        self.action_rules = {}
+        self.action_rules = []
         self.supp = supp
         self.conf = conf
         self.is_nan = is_nan
@@ -38,21 +37,22 @@ class ActionRules:
             if before == after and before != "nan":
                 return True, (before,), True
             if self.is_nan:
-                    if before == "nan" and after != "nan":
-                        return True, (after,), False
+                if before == "nan" and after != "nan":
+                    return True, (after,), False
         if attribute_type == "flexible":
             if before == "nan" and after == "nan":
                 return False, None, None
             if before != after and before != "nan" and after != "nan":
                 return True, (before, after), True
             if self.is_nan:
-                    if before != after and before == "nan":
-                        return True, (str(None), after), False
+                if before != after and before == "nan":
+                    return True, (str(None), after), False
         return False, None, None
 
     def _create_action_rules(self, couple, attribute_type):
         action_rule_part = tuple()
         has_supp_part = True
+        is_all = True
         for column in couple:
             is_action_couple, action_couple, has_supp = self._is_action_couple(
                 before=couple.at[0, column],
@@ -62,7 +62,10 @@ class ActionRules:
                 action_rule_part = action_rule_part + (column, action_couple)
                 if not has_supp:
                     has_supp_part = False
-        return action_rule_part, has_supp_part
+            else:
+                if str(couple.at[0, column]) != "nan" or str(couple.at[1, column]) != "nan":
+                    is_all = False
+        return action_rule_part, has_supp_part, is_all
 
     def _add_action_rule(self,
                          action_rule_stable,
@@ -70,18 +73,8 @@ class ActionRules:
                          action_rule_decision,
                          action_rule_supp,
                          action_rule_conf):
-        key = (action_rule_stable, action_rule_flexible, action_rule_decision)
-        if key in self.action_rules:
-            action_rule_supp_used, action_rule_conf_used = self.action_rules[key]
-            if (action_rule_supp[2] is not None and
-                    action_rule_supp[2] >= action_rule_supp_used[2] and
-                    action_rule_supp[1] is not None and
-                    action_rule_supp[1] >= action_rule_supp_used[1] and
-                    action_rule_supp[0] is not None and
-                    action_rule_supp[0] >= action_rule_supp_used[0]):
-                self.action_rules[key] = (action_rule_supp, action_rule_conf)
-        else:
-            self.action_rules[key] = (action_rule_supp, action_rule_conf)
+        action_rule = (action_rule_stable, action_rule_flexible, action_rule_decision)
+        self.action_rules.append((action_rule, (action_rule_supp, action_rule_conf), ))
 
     def fit(self):
         """
@@ -95,22 +88,26 @@ class ActionRules:
             conf = self.conf.pop(0)
             indexes = list(stable_columns.index.values)
             for comb in itertools.permutations(indexes, 2):
-                stable_couple = stable_columns.loc[list(comb)].reset_index(drop=True)
-                flexible_couple = flexible_columns.loc[list(comb)].reset_index(drop=True)
-                decision_couple = decision_column.loc[list(comb)].reset_index(drop=True)
-                supp_couple = supp.loc[list(comb)].reset_index(drop=True)
-                conf_couple = conf.loc[list(comb)].reset_index(drop=True)
-                if self.desired_state.is_candidate_couple(decision_couple):
-                    action_rule_stable, has_supp_stable = self._create_action_rules(stable_couple, "stable")
-                    action_rule_flexible, has_supp_flexible = self._create_action_rules(flexible_couple, "flexible")
-                    action_rule_decision = (decision_couple.iat[0, 0], decision_couple.iat[1, 0])
-                    if has_supp_stable and has_supp_flexible:
-                        action_rule_supp = (supp_couple[0], supp_couple[1], min(supp_couple[0], supp_couple[1]))
-                        action_rule_conf = (conf_couple[0], conf_couple[1], conf_couple[0] * conf_couple[1])
-                    else:
-                        action_rule_supp = (None, supp_couple[1], None)
-                        action_rule_conf = (None, conf_couple[1], None)
-                    if len(action_rule_flexible) > 0:
+                stable_couples = stable_columns.loc[list(comb)].reset_index(drop=True)
+                flexible_couples = flexible_columns.loc[list(comb)].reset_index(drop=True)
+                decision_couples = decision_column.loc[list(comb)].reset_index(drop=True)
+                supp_couples = supp.loc[list(comb)].reset_index(drop=True)
+                conf_couples = conf.loc[list(comb)].reset_index(drop=True)
+                if self.desired_state.is_candidate_couple(decision_couples):
+                    action_rule_stable, has_supp_stable, is_all_stable = self._create_action_rules(stable_couples,
+                                                                                                   "stable")
+                    action_rule_flexible, has_supp_flexible, is_all_flexible = self._create_action_rules(
+                        flexible_couples,
+                        "flexible")
+                    action_rule_decision = (
+                        decision_couples.columns[0], decision_couples.iat[0, 0], decision_couples.iat[1, 0])
+                    if len(action_rule_flexible) > 0 and is_all_stable and is_all_flexible:
+                        if has_supp_stable and has_supp_flexible:
+                            action_rule_supp = (supp_couples[0], supp_couples[1], min(supp_couples[0], supp_couples[1]))
+                            action_rule_conf = (conf_couples[0], conf_couples[1], conf_couples[0] * conf_couples[1])
+                        else:
+                            action_rule_supp = (None, supp_couples[1], None)
+                            action_rule_conf = (None, conf_couples[1], None)
                         self._add_action_rule(action_rule_stable,
                                               action_rule_flexible,
                                               action_rule_decision,
