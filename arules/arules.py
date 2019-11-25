b0e12c4c38f0bce2d90cda58849fe6740ce08f0a
Lukas Sykora <lukas.sykora@wundermanthompson.com>
2019-11-25 23:10:09 +0100

    New representation

--- arules/arules.py
+++ arules/arules.py
@@ -2,6 +2,7 @@ import pandas as pd
 from typing import List
 from typing import Union
 import itertools
+
 from ..desiredstate import DesiredState
 
 
@@ -19,7 +20,10 @@ class ActionRules:
                  conf: List[pd.Series],
                  is_nan: bool = False,
                  min_stable_antecedents: int = 1,
-                 min_flexible_antecedents: int = 1):
+                 min_flexible_antecedents: int = 1,
+                 max_stable_antecedents: int = 1,
+                 max_flexible_antecedents: int = 1
+                 ):
         """
         Initialise by reduced tables.
         """
@@ -29,11 +33,14 @@ class ActionRules:
         self.desired_state = desired_state
         self.action_rules = []
         self.action_rules_pretty_text = []
+        self.action_rules_representation = []
         self.supp = supp
         self.conf = conf
         self.is_nan = is_nan
         self.min_stable_antecedents = min_stable_antecedents
         self.min_flexible_antecedents = min_flexible_antecedents
+        self.max_stable_antecedents = max_stable_antecedents
+        self.max_flexible_antecedents = max_flexible_antecedents
         self.used_indexes = []
 
     def _is_action_couple(self,
@@ -49,41 +56,42 @@ class ActionRules:
         after = str(after)
         if attribute_type == "stable":
             if before == "nan" and after == "nan":
-                return False, None, None
-            if before == after and before != "nan":
-                return True, (before,), True
-            if self.is_nan:
+                return False, None, False
+            elif before == after and before != "nan":
+                return True, (before,), False
+            elif self.is_nan:
                 if before == "nan" and after != "nan":
                     return True, (after + "*",), False
-        if attribute_type == "flexible":
+        elif attribute_type == "flexible":
             if before == "nan" and after == "nan":
-                return False, None, None
-            if before != after and before != "nan" and after != "nan":
-                return True, (before, after), True
-            if self.is_nan:
+                return False, None, False
+            elif before != after and before != "nan" and after != "nan":
+                return True, (before, after), False
+            elif self.is_nan:
                 if before != after and before == "nan":
                     return True, (str(None), after), False
-        return False, None, None
+        return False, None, True
 
     def _create_action_rules(self, couple: pd.DataFrame, attribute_type: str) -> tuple:
         """
         Create action rules couples
         """
         action_rule_part = []
-        has_supp_part = True
+        count_antecedents = 0
         for column in couple:
-            is_action_couple, action_couple, has_supp = self._is_action_couple(
+            is_action_couple, action_couple, break_rule = self._is_action_couple(
                 before=couple.at[0, column],
                 after=couple.at[1, column],
                 attribute_type=attribute_type)
-            if is_action_couple:
+            if break_rule:
+                return False, None, None
+            elif is_action_couple:
+                count_antecedents += 1
                 action_rule_part.append([column, action_couple])
-                if not has_supp:
-                    has_supp_part = False
             else:
-                if str(couple.at[0, column]) != "nan" or str(couple.at[1, column]) != "nan":
-                    return None, None, False
-        return action_rule_part, has_supp_part, True
+                if action_couple is not None:
+                    action_rule_part.append([column, action_couple])
+        return True, action_rule_part, count_antecedents
 
     def _add_action_rule(self,
                          action_rule_stable: list,
@@ -118,23 +126,22 @@ class ActionRules:
                 supp_couples = supp.loc[list(comb)].reset_index(drop=True)
                 conf_couples = conf.loc[list(comb)].reset_index(drop=True)
                 if self.desired_state.is_candidate_couple(decision_couples):
-                    action_rule_stable, has_supp_stable, is_all_stable = self._create_action_rules(stable_couples,
-                                                                                                   "stable")
-                    action_rule_flexible, has_supp_flexible, is_all_flexible = self._create_action_rules(
+                    is_all_stable, action_rule_stable, counted_stable = self._create_action_rules(
+                        stable_couples,
+                        "stable")
+                    is_all_flexible, action_rule_flexible, counted_flexible = self._create_action_rules(
                         flexible_couples,
                         "flexible")
                     action_rule_decision = [
                         decision_couples.columns[0], [decision_couples.iat[0, 0], decision_couples.iat[1, 0]]]
                     if is_all_stable and \
                             is_all_flexible and \
-                            len(action_rule_flexible) >= self.min_flexible_antecedents and \
-                            len(action_rule_stable) >= self.min_stable_antecedents:
-                        if has_supp_stable and has_supp_flexible:
-                            action_rule_supp = [supp_couples[0], supp_couples[1], min(supp_couples[0], supp_couples[1])]
-                            action_rule_conf = [conf_couples[0], conf_couples[1], conf_couples[0] * conf_couples[1]]
-                        else:
-                            action_rule_supp = (None, supp_couples[1], None)
-                            action_rule_conf = (None, conf_couples[1], None)
+                            counted_flexible >= self.min_flexible_antecedents and \
+                            counted_stable >= self.min_stable_antecedents and \
+                            counted_flexible <= self.max_flexible_antecedents and \
+                            counted_stable <= self.max_stable_antecedents:
+                        action_rule_supp = [supp_couples[0], supp_couples[1], min(supp_couples[0], supp_couples[1])]
+                        action_rule_conf = [conf_couples[0], conf_couples[1], conf_couples[0] * conf_couples[1]]
                         self._add_action_rule(action_rule_stable,
                                               action_rule_flexible,
                                               action_rule_decision,
@@ -161,3 +168,27 @@ class ActionRules:
             text += "then '" + str(decision[0]) + "' value '" + str(decision[1][0]) + "' is changed to '" + \
                     str(decision[1][1]) + "' with support: " + str(supp[2]) + " and confidence: " + str(conf[2]) + "."
             self.action_rules_pretty_text.append(text)
+
+    def representation(self):
+        for row in self.action_rules:
+            action_rule = row[0]
+            supp = row[1]
+            conf = row[2]
+            text = "r = [   "
+            # Stable part
+            stable_part = action_rule[0]
+            text = text[:-3]
+            for stable_couple in stable_part:
+                text += "(" + str(stable_couple[0]) + ": " + str(stable_couple[1][0]) + ") ∧ "
+            # Flexible part
+            flexible_part = action_rule[1]
+            text = text[:-3]
+            for flexible_couple in flexible_part:
+                text += " ∧ (" + str(flexible_couple[0]) + ": " + str(flexible_couple[1][0]) + \
+                        " → " + str(flexible_couple[1][1]) + ") "
+            # Decision
+            decision = action_rule[2]
+            text += "] ⇒ [" + str(decision[0]) + ": " + str(decision[1][0]) + " → " + \
+                    str(decision[1][1]) + "] with support: " + str(supp[2]) + " and confidence: " + str(
+                conf[2])
+            self.action_rules_representation.append(text)
