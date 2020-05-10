22aa86614a0b3f466170ace5668bb85f1b2f05fb
Lukas Sykora <lukassykora@seznam.cz>
2020-05-10 19:39:37 +0200

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="actionrules-lukassykora",
-    version="1.1.16",
+    version="1.1.17",
     author="Lukas Sykora",
     author_email="lukassykora@seznam.cz",
     description="Action rules mining package",
